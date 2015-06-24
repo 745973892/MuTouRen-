beeeba9ce3ad6b63add268050ae00fb003250749
zhou <745973892@qq.com>
2015-06-24 09:57:08 +0800

    diyici

--- Git/AppDelegate.m
+++ Git/AppDelegate.m
@@ -20,6 +20,10 @@
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
+    
+    NSLog(@"fsafs");
+    
+    
     return YES;
 }
 
